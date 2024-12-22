2f87fea1430a36973826a7be8fd11fc5524e0bd4
Yoldoshali <esonaliyev77777@gmail.com>
2024-12-22 14:32:55 +0500

    Set Up Routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider, defaultSystem, Theme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
 import "./index.css";
 import { ColorModeProvider } from "./components/ui/color-mode.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,7 @@ createRoot(document.getElementById("root")!).render(
       <ColorModeProvider forcedTheme="dark">
         <Theme appearance="dark">
           <QueryClientProvider client={queryClient}>
-            <App />
+            <RouterProvider router={router} />
             <ReactQueryDevtools />
           </QueryClientProvider>
         </Theme>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,32 +5,26 @@ import {
   GridItem,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
 
-function App() {
-  // Determine if the screen size is above "lg"
-
+const HomePage = () => {
   const isAboveLg = useBreakpointValue({ base: false, lg: true });
 
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "250px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       {isAboveLg && (
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -48,6 +42,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
