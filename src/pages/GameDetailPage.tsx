56613e2ef496572abad5b9730293b6268ab3f615
Yoldoshali <esonaliyev77777@gmail.com>
2024-12-22 19:51:10 +0500

    Build Expandable Text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,18 +1,20 @@
+import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "./ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+
   if (isLoading) return <Spinner />;
 
-  if (error || !game) throw error;
+  if (error || !game) return <Text>Error loading game details</Text>;
 
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
